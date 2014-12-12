40cbfcde6064af7c17f165196d234a15acdaa767
garciadelcastillo <personal@garciadelcastillo.es>
2014-12-12 11:56:59 -0500

    walkthrough

--- docs/walkthrough/point_projections/sketch.js
+++ docs/walkthrough/point_projections/sketch.js
@@ -1,21 +1,20 @@
 // Create new instance of Sketchpad in target Canvas
 var pad = new Sketchpad('sketchpadCanvas');
 
-// Create some Nodes and join them with Lines
-var A = new pad.Node(100, 100),
-    B = new pad.Node(540, 300),
-    C = new pad.Node(540, 100),
-    D = new pad.Node(100, 300),
+// Create some Nodes, Lines and Circles
+var A = new pad.Node(80, 100),
+    B = new pad.Node(280, 300),
+    C = new pad.Node(460, 200),
+	P = new pad.Node(280, 100),
     AB = pad.Line.between(A, B),
-    CD = pad.Line.between(C, D);
+    circleC = new pad.Circle.centerRadius(C, 101);
 
-// Compute the intersection of the Lines, and attach a Circle to it
-var X = pad.Point.intersection(AB, CD),
-	circleX = pad.Circle.centerRadius(X, 101);
+// Compute the projection of P on the Line and Circle
+var PAB = pad.Point.projection(P, AB),
+	PC = pad.Point.projection(P, circleC);
 
-// Compute intersections between Circle and Lines
-var XAB = pad.Point.intersection(AB, circleX),
-	XCD = pad.Point.intersection(CD, circleX);
+// Create a Line between the projections
+var PABC = pad.Line.between(PAB, PC);
 
 // Display and Tag all Points
 pad.showPoints();
