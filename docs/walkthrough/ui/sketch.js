01288ce5f83d658b1a5b426deeba81c9e787a7fb
garciadelcastillo <personal@garciadelcastillo.es>
2015-01-16 21:42:09 +0100

    Tweaks

--- docs/walkthrough/ui/sketch.js
+++ docs/walkthrough/ui/sketch.js
@@ -1,3 +1,13 @@
+// Create new instance of Sketchpad in target Canvas
+var pad = new Sketchpad('sketchpadCanvas');
+
+// Create a Measure object through the Slider interface
+var radius = Slider(pad, 25, 25, 150, 0, 300, 150);
+
+// Now use the slider's Measure as radius for a circle
+var center = new pad.Node(pad.width.value / 2, pad.height.value / 2),
+    circle = pad.Circle.centerRadius(center, radius);
+
 /**
  * A quick implementation of a Slider class
  * Returns a Measure object defined by the slider's state
@@ -13,13 +23,3 @@ function Slider(pad, x, y, width, minValue, maxValue, startValue) {
       tag = new pad.Tag.on(handle, label);
   return measure;
 };
-
-// Create new instance of Sketchpad in target Canvas
-var pad = new Sketchpad('sketchpadCanvas');
-
-// Create a Measure object through the Slider interface
-var radius = Slider(pad, 25, 25, 150, 0, 300, 150);
-
-// Now use the slider's Measure as radius for a circle
-var center = new pad.Node(pad.width.value / 2, pad.height.value / 2),
-    circle = pad.Circle.centerRadius(center, radius);
